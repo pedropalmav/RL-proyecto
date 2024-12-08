e64fd9afde50cfe5c1519f645434078276987be7
Pedro Palma <pedro.palma@uc.cl>
2024-12-07 21:02:19 -0300

    feat: Initialize TaxiGrid

--- hurry_taxi/envs/taxi_grid.py
+++ hurry_taxi/envs/taxi_grid.py
@@ -2,23 +2,36 @@ import gymnasium as gym
 from gymnasium import spaces
 from gymnasium.error import DependencyNotInstalled
 import numpy as np
+from enum import Enum
 
 from hurry_taxi.utils.guaussian import Gaussian2D
 from hurry_taxi.utils.position_randomizer import PositionRandomizer
 
+class Actions(Enum):
+    right = 0
+    up = 1
+    left = 2
+    down = 3
+
 class TaxiGridEnv(gym.Env):
-    def __init__(self, grid_size=4, max_steps=100):
+    def __init__(self, grid_size=5, max_steps=100):
         self.grid_size = grid_size
         self.max_steps = max_steps
-        self.action_space = spaces.Discrete(4) # Up, Down, Left, Right
-        # TODO: Revisar este espacio de observación
+
+        # Up, Down, Left, Right
+        self.action_space = spaces.Discrete(4)
         self.observation_space = spaces.Box(low=0, high=grid_size, shape=(2, ), dtype=np.float32)
         self.state = None
 
+        self._init_randomizers()
+        self._init_visualization()
+    
+    def _init_randomizers(self):
         self.randomizer = PositionRandomizer(self.grid_size)
-        mean = grid_size / 2
+        mean = self.grid_size / 2
         self.gaussian = Gaussian2D([mean, mean], [[1, 0], [0, 1]])
 
+    def _init_visualization(self):
         self.render_mode = "human"
         self.screen_width = 600
         self.screen_height = 600
@@ -28,7 +41,7 @@ class TaxiGridEnv(gym.Env):
 
     def reset(self, seed=None, options=None):
         self.steps = 0
-        self.state = PositionRandomizer(self.grid_size).continuous_randomize()
+        self.state = PositionRandomizer(self.grid_size).discrete_randomize()
         return np.array(self.state, dtype=np.float32), {}
     
     def step(self, action):
