1c0594b9699442963a6255acdc8aa42c7fa446b1
Pedro Palma <pedro.palma@uc.cl>
2024-12-13 09:17:11 -0300

    feat: Add args to TD3

--- TD3.py
+++ TD3.py
@@ -3,18 +3,28 @@ from stable_baselines3 import TD3
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 import numpy as np
+import argparse
+import os
 import hurry_taxi
 
-env = gym.make("hurry_taxi/TaxiGrid-v0", max_steps = 1000, agents_number=4)
+parser = argparse.ArgumentParser()
+parser.add_argument("--steps", type=int, default=1000, required=False)
+parser.add_argument("--agents", type=int, default=4, required=False)
+parser.add_argument("--npcs", type=int, default=4, required=False)
+args = parser.parse_args()
+
+filename = f"td3_{args.steps}_{args.agents}_{args.npcs}"
+
+env = gym.make("hurry_taxi/TaxiGrid-v0", max_steps=args.steps, agents_number=args.agents, npc_number=args.npcs)
 
 n_actions = env.action_space.shape[-1]
 action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=0.01 * np.ones(n_actions))
 
-env = Monitor(env, filename='logs/td3')
+env = Monitor(env, filename=os.path.join("logs", filename))
 obs, info = env.reset()
 
 
 model = TD3("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=25000, log_interval=10)
-model.save("test_PPO_1")
+model.save(os.path.join("models", filename))
 
