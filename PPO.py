4ada26ed7374238e0dcee4f11d3141940989aa41
Pedro Palma <pedro.palma@uc.cl>
2024-12-13 09:09:45 -0300

    feat: Add args to ppo.py

--- PPO.py
+++ PPO.py
@@ -1,15 +1,26 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.monitor import Monitor
+import argparse
+import os
 
 import hurry_taxi
 
-env = gym.make("hurry_taxi/TaxiGrid-v0", max_steps = 1000, agents_number=4)
-env = Monitor(env, filename='logs/ppo')
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--steps", type=int, default=1000, required=False)
+parser.add_argument("--agents", type=int, default=4, required=False)
+parser.add_argument("--npcs", type=int, default=4, required=False)
+
+args = parser.parse_args()
+
+filename = f"ppo_{args.steps}_{args.agents}_{args.npcs}"
+
+env = gym.make("hurry_taxi/TaxiGrid-v0", max_steps=args.steps, agents_number=args.agents, npc_number=args.npcs)
+env = Monitor(env, filename=os.path.join("logs", filename))
 obs, info = env.reset()
 
 
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=25000)
+model.learn(total_timesteps=25000, log_interval=10)
 model.save("test_PPO_1")
-
