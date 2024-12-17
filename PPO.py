f8f78ffc2144db50f7d3ed2bee222b924cdaa1b1
Pedro Palma <pedro.palma@uc.cl>
2024-12-17 12:13:02 -0300

    fix: PPO warnings

--- PPO.py
+++ PPO.py
@@ -1,34 +1,43 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.monitor import Monitor
 import argparse
 import os
 
 import hurry_taxi
 
-parser = argparse.ArgumentParser()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-parser.add_argument("--steps", type=int, default=1000, required=False)
-parser.add_argument("--size", type=int, default=25, required=False, choices=[5, 10, 25])
-parser.add_argument("--agents", type=int, default=4, required=False)
-parser.add_argument("--npcs", type=int, default=4, required=False)
+    parser.add_argument("--steps", type=int, default=1000, required=False)
+    parser.add_argument(
+        "--size", type=int, default=25, required=False, choices=[5, 10, 25]
+    )
+    parser.add_argument("--agents", type=int, default=4, required=False)
+    parser.add_argument("--npcs", type=int, default=4, required=False)
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-filename = f"ppo_{args.size}_{args.steps}_{args.agents}_{args.npcs}"
+    filename = f"ppo_{args.size}_{args.steps}_{args.agents}_{args.npcs}"
 
-env = gym.make(
-    "hurry_taxi/TaxiGrid-v0", 
-    max_steps=args.steps, 
-    agents_number=args.agents, 
-    npc_number=args.npcs,
-    grid_size=args.size
-)
+    def make_env():
+        env = gym.make(
+            "hurry_taxi/TaxiGrid-v0",
+            max_steps=args.steps,
+            agents_number=args.agents,
+            npc_number=args.npcs,
+            grid_size=args.size,
+        )
+        return Monitor(env, filename=os.path.join("logs", filename))
 
-env = Monitor(env, filename=os.path.join("logs", filename))
-obs, info = env.reset()
+    env = make_vec_env(
+        make_env,
+        n_envs=1,
+        vec_env_cls=SubprocVecEnv,
+    )
 
-
-model = PPO("MlpPolicy", env, verbose=1, gamma=0.9)
-model.learn(total_timesteps=25*args.steps, log_interval=10)
-model.save(os.path.join("models", filename))
+    model = PPO("MlpPolicy", env, verbose=1, gamma=0.99, device="cpu")
+    model.learn(total_timesteps=25 * args.steps, log_interval=10)
+    model.save(os.path.join("models", filename))
