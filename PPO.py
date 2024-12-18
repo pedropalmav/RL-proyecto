a805b35938868507cc57e8a3ba534edd57afa4a2
Pedro Palma <pedro.palma@uc.cl>
2024-12-18 18:04:22 -0300

    feat: Create folder for PPO logs

--- PPO.py
+++ PPO.py
@@ -2,11 +2,22 @@ import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import SubprocVecEnv
-from stable_baselines3.common.monitor import Monitor
 import argparse
 import os
 
 import hurry_taxi
+
+
+def make_env():
+    env = gym.make(
+        "hurry_taxi/TaxiGrid-v0",
+        max_steps=args.steps,
+        agents_number=args.agents,
+        npc_number=args.npcs,
+        grid_size=args.size,
+    )
+    return env
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -20,25 +31,17 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    filename = f"ppo_{args.size}_{args.steps}_{args.agents}_{args.npcs}"
-
-    def make_env():
-        env = gym.make(
-            "hurry_taxi/TaxiGrid-v0",
-            max_steps=args.steps,
-            agents_number=args.agents,
-            npc_number=args.npcs,
-            grid_size=args.size,
-        )
-        return env
+    model_name = f"ppo_{args.size}_{args.steps}_{args.agents}_{args.npcs}"
+    folder_name = os.path.join("models", model_name)
+    os.makedirs(folder_name, exist_ok=True)
 
     env = make_vec_env(
         make_env,
         n_envs=8,
         vec_env_cls=SubprocVecEnv,
-        monitor_dir="logs",
+        monitor_dir=folder_name,
     )
 
     model = PPO("MlpPolicy", env, verbose=1, gamma=0.99, device="cpu")
     model.learn(total_timesteps=2000000, log_interval=10)
-    model.save(os.path.join("models", filename))
+    model.save(os.path.join("models", model_name))
