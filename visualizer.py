bc5d2e58a649821d12ec3c32f08406c469e70412
Pedro Palma <pedro.palma@uc.cl>
2024-12-20 10:06:53 -0300

    feat: Add logs to visualizer

--- visualizer.py
+++ visualizer.py
@@ -1,7 +1,20 @@
 import hurry_taxi
+from hurry_taxi.envs.taxi_grid import Actions
+import numpy as np
 import os
 import gymnasium as gym
 from stable_baselines3 import PPO
+
+
+def parse_action(actions):
+    discrete_actions = []
+    for action in actions:
+        discrete_action = int(
+            np.clip((action + 1) * (len(Actions) - 1) / 2, 0, len(Actions) - 1)
+        )
+        discrete_actions.append(discrete_action)
+    return [Actions(action) for action in discrete_actions]
+
 
 steps = 5000
 size = 5
@@ -17,12 +30,20 @@ env = gym.make(
     npc_number=npcs,
     grid_size=size,
     render_mode="human",
+    max_episode_steps=steps,
 )
 
 obs, info = env.reset()
+i = 0
+cumulative_reward = 0
 while True:
     action, _states = model.predict(obs, deterministic=True)
     obs, rewards, terminated, truncated, info = env.step(action)
+    cumulative_reward += rewards
+    i += 1
     env.render()
+    print(i, parse_action(action), cumulative_reward)
     if terminated:
         break
+
+env.close()
